af839e88c4549670ed7178eecb5810c09baa17bd
Henry Agustin <henryagustin297@gmail.com>
2023-02-20 16:38:49 -0600

    Aprendimos a renderizar rutas dinamicas con useParams

---
+++ src/blogdata.js
@@ -0,0 +1,21 @@
+const blogdata = [];
+blogdata.push({
+    title: "Qué es React",
+    slug: "que-es-react",
+    content: "React es el mejor framework de Js.",
+    author: "henry1227"
+});
+blogdata.push({
+    title: "Qué es Vue",
+    slug: "que-es-vue",
+    content: "Vue es el mejor framework de Js.",
+    author: "alex127"
+});
+blogdata.push({
+    title: "Qué es Angular",
+    slug: "que-es-angular",
+    content: "Angular es el mejor framework de Js.",
+    author: "lex241"
+});
+
+export { blogdata }
